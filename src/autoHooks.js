0e10dddaebf4a501accbec835fbeb6ec1b7ffbe6
Kalomika <kalomikafilms@gmail.com>
2026-06-27 18:17:36 -0500

    Tick fresh air and upstairs view hold

--- src/autoHooks.js
+++ src/autoHooks.js
@@ -1,8 +1,11 @@
+import { updateFreshAir } from './blueprint.js';
 import { updateAppointments } from './buildRequests.js';
 import { updateMoveJob } from './objectMove.js';
 import { runRoutines, updateTraining } from './training.js';
 
 export function updateAutoHooks(state, dt) {
+  if (state.viewHoldT > 0) state.viewHoldT = Math.max(0, state.viewHoldT - dt);
+  updateFreshAir(state, dt);
   updateTraining(state);
   updateMoveJob(state);
   updateAppointments(state, dt);
