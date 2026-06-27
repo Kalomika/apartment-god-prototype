5c54b4037b030d2ad8c430539e94bf49f0a244c9
Kalomika <kalomikafilms@gmail.com>
2026-06-27 18:19:03 -0500

    Tick staged cooking flow

--- src/autoHooks.js
+++ src/autoHooks.js
@@ -1,11 +1,13 @@
 import { updateFreshAir } from './blueprint.js';
 import { updateAppointments } from './buildRequests.js';
+import { updateCooking } from './cooking.js';
 import { updateMoveJob } from './objectMove.js';
 import { runRoutines, updateTraining } from './training.js';
 
 export function updateAutoHooks(state, dt) {
   if (state.viewHoldT > 0) state.viewHoldT = Math.max(0, state.viewHoldT - dt);
   updateFreshAir(state, dt);
+  updateCooking(state);
   updateTraining(state);
   updateMoveJob(state);
   updateAppointments(state, dt);
