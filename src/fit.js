36deaf1161b7f6930e4646154899a74d5d8baf26
Kalomika <kalomikafilms@gmail.com>
2026-06-27 18:10:58 -0500

    Use 960x720 playable canvas fit

--- src/fit.js
+++ src/fit.js
@@ -5,6 +5,8 @@ const canvas = document.getElementById('game');
 
 function fit() {
   const wide = window.innerWidth > window.innerHeight && window.innerWidth >= 760;
+  canvas.width = 960;
+  canvas.height = 720;
   shell.style.display = 'flex';
   shell.style.flexDirection = wide ? 'row' : 'column';
   shell.style.width = '100vw';
@@ -13,21 +15,22 @@ function fit() {
   wrap.style.alignItems = 'center';
   wrap.style.justifyContent = 'center';
   wrap.style.overflow = 'hidden';
-  canvas.style.width = 'auto';
   canvas.style.height = 'auto';
   canvas.style.maxWidth = '100%';
   canvas.style.maxHeight = '100%';
-  canvas.style.aspectRatio = '16 / 9';
+  canvas.style.aspectRatio = '4 / 3';
   if (wide) {
     wrap.style.flex = '1 1 auto';
     wrap.style.height = '100vh';
     hud.style.flex = '0 0 360px';
     hud.style.height = '100vh';
+    canvas.style.width = 'min(calc(100vw - 360px), calc(100vh * 4 / 3))';
   } else {
     wrap.style.flex = '0 0 50vh';
     wrap.style.height = '50vh';
     hud.style.flex = '1 1 auto';
     hud.style.height = '';
+    canvas.style.width = 'min(100vw, calc(50vh * 4 / 3))';
   }
 }
 
