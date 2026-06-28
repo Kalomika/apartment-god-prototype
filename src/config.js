35c20f1ec2296f93c0b802beb0f6a0802b31936a
Kalomika <kalomikafilms@gmail.com>
2026-06-27 23:46:16 -0500

    Use emoji moods and add relationship actions

--- src/config.js
+++ src/config.js
@@ -16,8 +16,8 @@ export const NEEDS = [
 ];
 
 export const MOODS = {
-  neutral: ':)', happy: ':D', calm: '=)', tired: 'Zz', hungry: 'FO', stinky: 'ew',
-  love: '<3', playful: ';)', spooked: '!!', hyped: 'GO', phone: 'PH', dog: 'wo'
+  neutral: '🙂', happy: '😄', calm: '😌', tired: '😴', hungry: '😋', stinky: '🤢',
+  love: '😍', playful: '😉', spooked: '😱', hyped: '🔥', phone: '📱', dog: '🐶'
 };
 
 export const COLORS = {
@@ -27,27 +27,30 @@ export const COLORS = {
 };
 
 export const ACTIONS = {
-  couch: [['watch_tv', 'Watch TV'], ['relax', 'Relax'], ['nap', 'Nap']],
-  tv: [['watch_tv', 'Watch TV'], ['comedy', 'Comedy Channel'], ['horror', 'Horror Channel'], ['sports', 'Sports Channel']],
+  couch: [['watch_tv', 'Watch TV'], ['watch_together', 'Watch TV Together'], ['relax', 'Relax'], ['nap', 'Nap']],
+  tv: [['watch_tv', 'Watch TV'], ['watch_together', 'Watch TV Together'], ['comedy', 'Comedy Channel'], ['horror', 'Horror Channel'], ['sports', 'Sports Channel']],
   fridge: [['snack', 'Get Snack'], ['meal', 'Cook Meal'], ['bring_food', 'Bring Food']],
   stove: [['meal', 'Cook Meal']],
   sink: [['clean', 'Clean'], ['brush_teeth', 'Brush Teeth'], ['groom', 'Groom']],
   shower: [['shower', 'Take Shower'], ['groom', 'Groom']],
   toilet: [['toilet', 'Use Toilet']],
   door: [['work', 'Work'], ['errand', 'Quick Errand'], ['mall', 'Mall Trip'], ['movies', 'Movie Theater'], ['date', 'Date Night']],
-  bed: [['sleep', 'Sleep'], ['relax', 'Relax']],
+  bed: [['sleep', 'Sleep'], ['bed_together', 'Go To Bed Together'], ['intimacy', 'Private Moment'], ['relax', 'Relax']],
   desk: [['desk_work', 'Work at Desk'], ['play_game', 'Play Game'], ['phone', 'Phone'], ['shop', 'Shop']],
   dog_bowl: [['feed_dog', 'Feed Dog']],
   light: [['toggle_light', 'Toggle Room Light']]
 };
 
-export const SOCIAL_ACTIONS = [['talk', 'Talk'], ['kiss', 'Kiss'], ['cuddle', 'Cuddle'], ['tickle', 'Tickle'], ['hands', 'Hold Hands']];
+export const SOCIAL_ACTIONS = [
+  ['talk', 'Talk'], ['kiss', 'Kiss'], ['cuddle', 'Cuddle'], ['tickle', 'Tickle'], ['hands', 'Hold Hands'],
+  ['watch_together', 'Watch TV Together'], ['bed_together', 'Go To Bed Together'], ['intimacy', 'Private Moment']
+];
 export const DOG_SOCIAL_ACTIONS = [['pet', 'Pet Dog'], ['train', 'Train Dog'], ['fetch', 'Fetch']];
 
 export const ACTION_TIMES = {
-  watch_tv: 7, comedy: 7, horror: 7, sports: 7, relax: 5, nap: 8, snack: 4, meal: 7,
+  watch_tv: 7, watch_together: 9, comedy: 7, horror: 7, sports: 7, relax: 5, nap: 8, snack: 4, meal: 7,
   bring_food: 6, clean: 6, brush_teeth: 4, groom: 5, shower: 7, toilet: 4, sleep: 12,
-  desk_work: 8, play_game: 7, phone: 5, shop: 5, feed_dog: 3, toggle_light: 1,
+  bed_together: 12, intimacy: 14, desk_work: 8, play_game: 7, phone: 5, shop: 5, feed_dog: 3, toggle_light: 1,
   talk: 4, kiss: 3, cuddle: 6, tickle: 3, hands: 4, pet: 3, train: 5, fetch: 0,
   work: 14, errand: 9, mall: 12, movies: 13, date: 14
 };
