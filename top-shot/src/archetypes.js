8bb44e31a1e8809b89480edb2c2b9d265e1ea4e0
Kalomika <kalomikafilms@gmail.com>
2026-06-28 01:36:24 -0500

    Add Top Shot fighter archetypes

---
+++ top-shot/src/archetypes.js
@@ -0,0 +1,26 @@
+export const ARCHETYPES = {
+  marine: {
+    id: 'marine', name: 'Marine', color: '#6da3ff', accent: '#253b5f', weapon: 'rifle', melee: 'knife', special: 'prone',
+    stats: { speed: 108, stealth: 38, sight: 82, hearing: 65, aim: 82, cqc: 62, block: 68, dodge: 48, counter: 52, grapple: 66, toughness: 88, stamina: 84, aggression: 62, recovery: 60 },
+    resources: { rifle: 54, pistol: 18, heat: 0, special: 1 }
+  },
+  ninja: {
+    id: 'ninja', name: 'Ninja', color: '#8d92a4', accent: '#262a36', weapon: 'shuriken', melee: 'sword', special: 'smoke',
+    stats: { speed: 152, stealth: 94, sight: 74, hearing: 82, aim: 62, cqc: 84, block: 58, dodge: 88, counter: 76, grapple: 54, toughness: 54, stamina: 92, aggression: 76, recovery: 72 },
+    resources: { shuriken: 10, smoke: 3, heat: 0, special: 3 }
+  },
+  archer: {
+    id: 'archer', name: 'Archer', color: '#71d2a2', accent: '#284935', weapon: 'bow', melee: 'arrow_stab', special: 'roll',
+    stats: { speed: 124, stealth: 62, sight: 86, hearing: 70, aim: 78, cqc: 54, block: 52, dodge: 68, counter: 48, grapple: 42, toughness: 62, stamina: 80, aggression: 50, recovery: 66 },
+    resources: { arrows: 20, heat: 0, special: 2 }
+  },
+  martial_artist: {
+    id: 'martial_artist', name: 'Martial Artist', color: '#ffb15f', accent: '#623818', weapon: 'debris', melee: 'hands', special: 'focus',
+    stats: { speed: 136, stealth: 58, sight: 72, hearing: 78, aim: 48, cqc: 96, block: 88, dodge: 78, counter: 92, grapple: 84, toughness: 78, stamina: 90, aggression: 76, recovery: 78 },
+    resources: { debris: 4, heat: 0, special: 2 }
+  }
+};
+
+export function archetypeList() {
+  return Object.values(ARCHETYPES);
+}
