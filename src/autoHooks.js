bc83a9b55652c2ea8885eb7cdc50e508336930ec
Kalomika <kalomikafilms@gmail.com>
2026-07-04 04:04:24 -0500

    Run visible delivery updates

--- src/autoHooks.js
+++ src/autoHooks.js
@@ -1,6 +1,7 @@
 import { updateFreshAir } from './blueprint.js';
 import { updateAppointments } from './buildRequests.js';
 import { updateCooking } from './cooking.js';
+import { updateDelivery } from './economy.js';
 import { updateFetch } from './fetchSystem.js';
 import { updateMusic } from './music.js';
 import { updateMoveJob } from './objectMove.js';
@@ -10,6 +11,7 @@ export function updateAutoHooks(state, dt) {
   if (state.viewHoldT > 0) state.viewHoldT = Math.max(0, state.viewHoldT - dt);
   updateFreshAir(state, dt);
   updateCooking(state);
+  updateDelivery(state, dt);
   updateFetch(state);
   updateMusic(state);
   updateTraining(state);
