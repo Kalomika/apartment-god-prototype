b4342a3cf17c6a42a8bdd8caf50f75bf5d2628b9
Kalomika <kalomikafilms@gmail.com>
2026-07-06 04:24:45 -0500

    Prioritize current game clone on Phaser branch

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-async function boot() {
-  try {
-    const { bootPhaserGame } = await import('./phaserRuntime.js');
-    bootPhaserGame();
-  } catch (error) {
-    console.error('Phaser start problem. Loading visual parity mode.', error);
-    const { bootCanvasGame } = await import('./canvasRuntime.js');
-    bootCanvasGame(error?.message || 'Phaser start problem');
-  }
-}
+import { bootCanvasGame } from './canvasRuntime.js';
 
-boot();
+bootCanvasGame('clone-first mode');
