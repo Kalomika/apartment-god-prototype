a304a59dd812e14f7437e0743b9cf6b0df3be6cb
Kalomika <kalomikafilms@gmail.com>
2026-07-08 00:14:34 -0500

    Remove sliding door overlay renderer from world draw

--- src/renderWorld.js
+++ src/renderWorld.js
@@ -1,8 +1,6 @@
 import { doorways, windows } from './blueprint.js';
 import { drawStyledWorld } from './renderHouseStyle.js';
-import { drawSlidingDoorOverlays } from './renderSlidingDoors.js';
 
 export function drawWorld(ctx, state) {
   drawStyledWorld(ctx, state, doorways, windows);
-  drawSlidingDoorOverlays(ctx, state, doorways);
 }
