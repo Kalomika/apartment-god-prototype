c94c56c5966c11fc6d041b9194f24b34a734d373
Kalomika <kalomikafilms@gmail.com>
2026-07-08 00:08:00 -0500

    Render sliding door overlays over doorway symbols

--- src/renderWorld.js
+++ src/renderWorld.js
@@ -1,6 +1,8 @@
 import { doorways, windows } from './blueprint.js';
 import { drawStyledWorld } from './renderHouseStyle.js';
+import { drawSlidingDoorOverlays } from './renderSlidingDoors.js';
 
 export function drawWorld(ctx, state) {
   drawStyledWorld(ctx, state, doorways, windows);
+  drawSlidingDoorOverlays(ctx, state, doorways);
 }
