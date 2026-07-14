a0a1c79a85382498bd41cdd7458099072b6ac0b2
Kalomika <kalomikafilms@gmail.com>
2026-07-14 14:23:46 -0500

    Cache bust runtime for dining kitchen layout mirror

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import { bootCanvasGame } from './canvasRuntime.js?v=20260714-human-renderer-framework';
+import { bootCanvasGame } from './canvasRuntime.js?v=20260714-dining-kitchen-layout';
 
 bootCanvasGame();
