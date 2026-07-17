509c7286712e1e8212c75901427e4bef2417b8a7
Kalomika <kalomikafilms@gmail.com>
2026-07-17 18:34:49 -0500

    Mark game canvas as Phaser owned before renderer import

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './runtimeObjectCorrections.js?v=20260717-full-phaser-parity';
-import { bootPhaserParityGame } from './phaserParityRuntime.js?v=20260717-full-phaser-parity';
 
+const gameCanvas = document.getElementById('game');
+if (gameCanvas) gameCanvas.dataset.phaserOwned = 'true';
+
+const { bootPhaserParityGame } = await import('./phaserParityRuntime.js?v=20260717-full-phaser-parity');
 bootPhaserParityGame();
