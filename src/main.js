51f8705d17f260e25058c23c67ce02bc1406d4a1
Kalomika <kalomikafilms@gmail.com>
2026-07-17 18:23:11 -0500

    Boot the complete game through the Phaser parity runtime

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './runtimeObjectCorrections.js?v=20260716-layer-vehicle-games';
-import { bootCanvasGame } from './canvasRuntime.js?v=20260716-layer-vehicle-games';
+import './runtimeObjectCorrections.js?v=20260717-full-phaser-parity';
+import { bootPhaserParityGame } from './phaserParityRuntime.js?v=20260717-full-phaser-parity';
 
-bootCanvasGame();
+bootPhaserParityGame();
