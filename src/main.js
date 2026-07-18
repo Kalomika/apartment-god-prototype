7408a4665a92c4ab7432312949d530300f080164
Kalomika <kalomikafilms@gmail.com>
2026-07-18 04:52:11 -0500

    Install Phaser regression correction layer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import './runtimeObjectCorrections.js?v=20260717-full-phaser-parity';
+import './runtimeObjectCorrections.js?v=20260718-full-phaser-regression-repair';
+import { installPhaserParityCorrections } from './phaserParityCorrections.js?v=20260718-full-phaser-regression-repair';
 
 const gameCanvas = document.getElementById('game');
 if (gameCanvas) gameCanvas.dataset.phaserOwned = 'true';
 
-const { bootPhaserParityGame } = await import('./phaserParityRuntime.js?v=20260717-full-phaser-parity');
-bootPhaserParityGame();
+const { bootPhaserParityGame } = await import('./phaserParityRuntime.js?v=20260718-full-phaser-regression-repair');
+const game = await bootPhaserParityGame();
+installPhaserParityCorrections(game);
