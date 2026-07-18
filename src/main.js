bc6ac267dc207309abb031451cfd074a6a69382c
Kalomika <kalomikafilms@gmail.com>
2026-07-18 04:57:45 -0500

    Install Phaser visual parity overlay

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './runtimeObjectCorrections.js?v=20260718-full-phaser-regression-repair';
 import { installPhaserParityCorrections } from './phaserParityCorrections.js?v=20260718-full-phaser-regression-repair';
+import { installPhaserVisualParityOverlay } from './phaserVisualParityOverlay.js?v=20260718-full-phaser-regression-repair';
 
 const gameCanvas = document.getElementById('game');
 if (gameCanvas) gameCanvas.dataset.phaserOwned = 'true';
@@ -7,3 +8,4 @@ if (gameCanvas) gameCanvas.dataset.phaserOwned = 'true';
 const { bootPhaserParityGame } = await import('./phaserParityRuntime.js?v=20260718-full-phaser-regression-repair');
 const game = await bootPhaserParityGame();
 installPhaserParityCorrections(game);
+installPhaserVisualParityOverlay(game);
